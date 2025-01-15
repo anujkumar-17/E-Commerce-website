62181fb3f71b87e49a52d414d377c72ceb0a96e1
Anuj <anujk@maqsoftware.com>
2025-01-15 16:30:09 +0530

    final commit

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0b5a675dd86addfbfa0ac6dd36083b678a6888")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb17516f3e7473a2abbbc5f7910f66ad0204a9bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
